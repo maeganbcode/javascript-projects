d9a8333e4133c6db3af5e953320992d8e821c2b5
Maeganbcode <hairstylistmae@gmail.com>
2024-08-05 14:04:18 -0500

    domExercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,7 +3,21 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
+    button.addEventListener("click", event => {
+        paragraph.innerHTML = "Houston! We have liftoff!";
+    });
+    
+    missionAbort.addEventListener("mouseout", function( event ) {
+        event.target.style.backgroundColor = "";
+     });
+
+     missionAbort.addEventListener("click", function(event) {
+        const confirmAbort = window.confirm("Are you sure you want to abort the mission?");
+        if (confirmAbort) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+
+        }
+     });
     
 }
 
